982084b1ca98c8a643149b1f730fd95a192c68ea
Sneha Irukuvajjula <snehairukuvajjula@gmail.com>
2024-10-21 14:28:49 -0600

    Added r8 configuration

--- website/website.py
+++ website/website.py
@@ -184,7 +184,8 @@ def add_device():
         subprocess.run(["git", "add", "."], cwd="/home/student/git/csci5840")
         subprocess.run(["git", "commit", "-m", f"Added {device['name']} configuration"], cwd="/home/student/git/csci5840")
         subprocess.run(["git", "push"], cwd="/home/student/git/csci5840")
-        time.sleep(20)
+        
+        time.sleep(30)
         return jsonify({"status": "File saved, pushed to Github and Jenkins triggered."})
 
         # Redirect to home or success page
